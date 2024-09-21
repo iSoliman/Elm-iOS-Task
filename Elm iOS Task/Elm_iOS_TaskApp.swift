bfb0b87c710bd93af832e81699a9c9a454af3d94
islam Soliman <islam.seliman92@gmail.com>
2024-09-21 17:31:24 +0300

    Initial Commit

---
+++ Elm iOS Task/Elm_iOS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elm_iOS_TaskApp.swift
+//  Elm iOS Task
+//
+//  Created by Islam Soliman on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Elm_iOS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
